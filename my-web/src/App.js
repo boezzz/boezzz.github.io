afd7fdb8193322e829c67ddd744e9b2938c7d69c
Boe Zhou <55188595+boezzz@users.noreply.github.com>
2024-12-28 21:40:59 -0800

    change to hashed routing

--- my-web/src/App.js
+++ my-web/src/App.js
@@ -1,74 +1,86 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
-  ScrollRestoration,
+  useLocation,
+  Outlet,
 } from "react-router-dom";
 
-// import the pages here
+// Import the pages here
 import NotFoundPage from './pages/NotFoundPage';
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Resume from './pages/Resume';
 import Hidden from './pages/Hidden';
 
-// import all project pages
+// Import all project pages
 import Beatbounce from './pages/blog_pages/Beatbounce';
 import Forward_to_past from './pages/blog_pages/Forward_to_past';
 import Unspoken from './pages/blog_pages/Unspoken';
 import FridayHarbor from './pages/blog_pages/friday_harbor';
 
-// Wrapper component to include ScrollRestoration
-const PageWrapper = ({ children }) => {
-  return (
-    <>
-      {children}
-      <ScrollRestoration />
-    </>
-  );
+// ScrollToTop component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 };
 
-const router = createBrowserRouter([
-  {
-    // main pages, do not change those paths
-    path: "/",
-    element: <PageWrapper><Home /></PageWrapper>,
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: "/blog",
-    element: <PageWrapper><Blogs /></PageWrapper>,
-  },
-  {
-    path: "/resume",
-    element: <PageWrapper><Resume /></PageWrapper>,
-  },
-  {
-    path: "/hidden",
-    element: <PageWrapper><Hidden /></PageWrapper>,
-  },
+// Layout component to include ScrollToTop
+const Layout = () => (
+  <>
+    <ScrollToTop />
+    <Outlet />
+  </>
+);
 
-  // blog pages, add pages below
+const router = createHashRouter([
   {
-    path: "/beatbounce",
-    element: <PageWrapper><Beatbounce /></PageWrapper>,
-  },
-  {
-    path: "/forward-to-past",
-    element: <PageWrapper><Forward_to_past /></PageWrapper>,
-  },
-  {
-    path: "/xr-cooking-capstone",
-    element: <PageWrapper><Blogs /></PageWrapper>,
-  },
-  {
-    path: "/unspoken",
-    element: <PageWrapper><Unspoken /></PageWrapper>,
-  },
-  {
-    path: "/friday-harbor-2024",
-    element: <PageWrapper><FridayHarbor /></PageWrapper>,
+    element: <Layout />, // Add the layout with ScrollToTop here
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/blog",
+        element: <Blogs />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
+      },
+      {
+        path: "/hidden",
+        element: <Hidden />,
+      },
+      {
+        path: "/beatbounce",
+        element: <Beatbounce />,
+      },
+      {
+        path: "/forward-to-past",
+        element: <Forward_to_past />,
+      },
+      {
+        path: "/xr-cooking-capstone",
+        element: <Blogs />,
+      },
+      {
+        path: "/unspoken",
+        element: <Unspoken />,
+      },
+      {
+        path: "/friday-harbor-2024",
+        element: <FridayHarbor />,
+      },
+    ],
   },
 ]);
 
